25aa3f16d1a205e2a9fc24c57504256e9ee31ae2
lothGit <49041968+lothGit@users.noreply.github.com>
2026-03-14 16:45:27 +0100

    mise jour dur projet

--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,105 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { useState, useEffect } from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import { MainCard } from "../components/MainCard";
+import { ContentBox } from "../components/ContentBox";
+import { Header } from "../components/Header";
+import { DateAndTime } from "../components/DateAndTime";
+import { Search } from "../components/Search";
+import { MetricsBox } from "../components/MetricsBox";
+import { UnitSwitch } from "../components/UnitSwitch";
+import { LoadingScreen } from "../components/LoadingScreen";
+import { ErrorScreen } from "../components/ErrorScreen";
+import styles from "../styles/Home.module.css";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const App = () => {
+  const [cityInput, setCityInput] = useState("Riga");
+  const [triggerFetch, setTriggerFetch] = useState(true);
+  const [weatherData, setWeatherData] = useState();
+  const [unitSystem, setUnitSystem] = useState("metric");
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the index.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs/pages/getting-started?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("api/data", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ cityInput }),
+      });
+      const data = await res.json();
+      setWeatherData({ ...data });
+      setCityInput("");
+    };
+    getData();
+  }, [triggerFetch]);
+  const changeSystem = () =>
+    unitSystem == "metric"
+      ? setUnitSystem("imperial")
+      : setUnitSystem("metric");
+
+  return weatherData && !weatherData.message ? (
+    <div className={styles.wrapper}>
+      <MainCard
+        city={weatherData.name}
+        country={weatherData.sys.country}
+        description={weatherData.weather[0].description}
+        iconName={weatherData.weather[0].icon}
+        unitSystem={unitSystem}
+        weatherData={weatherData}
+      />
+      <ContentBox>
+       <Header>
+          <DateAndTime weatherData={weatherData} unitSystem={unitSystem} />
+          <Search
+            placeHolder="Search a city..."
+            value={cityInput}
+            onFocus={(e) => {
+              e.target.value = "";
+              e.target.placeholder = "";
+            }}
+            onChange={(e) => setCityInput(e.target.value)}
+            onKeyDown={(e) => {
+              e.keyCode === 13 && setTriggerFetch(!triggerFetch);
+              e.target.placeholder = "Search a city...";
+            }}
+          />
+        </Header>
+        <MetricsBox weatherData={weatherData} unitSystem={unitSystem} />
+        <UnitSwitch onClick={changeSystem} unitSystem={unitSystem} />
+      </ContentBox>
+      
     </div>
+  ) : weatherData && weatherData.message ? (
+    <ErrorScreen errorMessage="City not found, try again!">
+      <Search
+        onFocus={(e) => (e.target.value = "")}
+        onChange={(e) => setCityInput(e.target.value)}
+        onKeyDown={(e) => e.keyCode === 13 && setTriggerFetch(!triggerFetch)}
+      />
+    </ErrorScreen>
+  ) : (
+    <LoadingScreen loadingMessage="Loading data..." />
   );
+
+  /*
+const [age, setAge] = useState(null);
+useEffect(() => {
+const fetchAge = async () => {
+const response = await fetch('https://api.agify.io?name=john');
+const data = await response.json();
+setAge(data.age);
+console.log(data)
+};
+
+const fetchElevation=async ()=>{
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.4');
+const data = await response.json();
+console.log(data)
 }
+
+  fetchAge();
+  fetchElevation()
+
+}, []);
+  
+  return(<div>Age estimé pour John : {age}</div>)*/
+};
+export default App;
